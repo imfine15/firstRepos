ec22c6a6efd2c85deb38b97cd7dc2bda86a578e9
omytz@hrvbb.onmicrosoft.com <omytz@hrvbb.onmicrosoft.com>
2021-02-04 10:28:34 +0900

    프로젝트생성

---
+++ test3/test3/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {   
+            //컨트롤의 생성 등을 통해 폼을 생성해주는 메소드
+            InitializeComponent();
+        }
+    }
+}
